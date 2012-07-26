3cb39c212f0fe4550ca2e0936c72d4648d6bb311
Fogliarini Brolesi <fabio@fabio.mat.br>
2012-07-26 17:55:12 -0300

    Primeiro Commit

---
+++ cards/json-cartas.js
@@ -0,0 +1,262 @@
+[
+    {
+        "valor": "8",
+        "naipe": 1,
+        "classe": "card-8-copas"
+    },
+    {
+        "valor": "13",
+        "naipe": 0,
+        "classe": "card-k-ouros"
+    },
+    {
+        "valor": "12",
+        "naipe": 2,
+        "classe": "card-q-espadas"
+    },
+    {
+        "valor": "4",
+        "naipe": 1,
+        "classe": "card-4-copas"
+    },
+    {
+        "valor": "1",
+        "naipe": 0,
+        "classe": "card-a-ouros"
+    },
+    {
+        "valor": "12",
+        "naipe": 3,
+        "classe": "card-q-paus"
+    },
+    {
+        "valor": "10",
+        "naipe": 1,
+        "classe": "card-10-copas"
+    },
+    {
+        "valor": "7",
+        "naipe": 3,
+        "classe": "card-7-paus"
+    },
+    {
+        "valor": "5",
+        "naipe": 1,
+        "classe": "card-5-copas"
+    },
+    {
+        "valor": "2",
+        "naipe": 0,
+        "classe": "card-2-ouros"
+    },
+    {
+        "valor": "1",
+        "naipe": 2,
+        "classe": "card-a-espadas"
+    },
+    {
+        "valor": "11",
+        "naipe": 2,
+        "classe": "card-j-espadas"
+    },
+    {
+        "valor": "2",
+        "naipe": 1,
+        "classe": "card-2-copas"
+    },
+    {
+        "valor": "9",
+        "naipe": 2,
+        "classe": "card-9-espadas"
+    },
+    {
+        "valor": "4",
+        "naipe": 3,
+        "classe": "card-4-paus"
+    },
+    {
+        "valor": "12",
+        "naipe": 1,
+        "classe": "card-q-copas"
+    },
+    {
+        "valor": "4",
+        "naipe": 2,
+        "classe": "card-4-espadas"
+    },
+    {
+        "valor": "3",
+        "naipe": 1,
+        "classe": "card-3-copas"
+    },
+    {
+        "valor": "6",
+        "naipe": 0,
+        "classe": "card-6-ouros"
+    },
+    {
+        "valor": "8",
+        "naipe": 2,
+        "classe": "card-8-espadas"
+    },
+    {
+        "valor": "6",
+        "naipe": 1,
+        "classe": "card-6-copas"
+    },
+    {
+        "valor": "11",
+        "naipe": 0,
+        "classe": "card-j-ouros"
+    },
+    {
+        "valor": "8",
+        "naipe": 0,
+        "classe": "card-8-ouros"
+    },
+    {
+        "valor": "5",
+        "naipe": 3,
+        "classe": "card-5-paus"
+    },
+    {
+        "valor": "3",
+        "naipe": 3,
+        "classe": "card-3-paus"
+    },
+    {
+        "valor": "10",
+        "naipe": 0,
+        "classe": "card-10-ouros"
+    },
+    {
+        "valor": "7",
+        "naipe": 1,
+        "classe": "card-7-copas"
+    },
+    {
+        "valor": "2",
+        "naipe": 2,
+        "classe": "card-2-espadas"
+    },
+    {
+        "valor": "6",
+        "naipe": 2,
+        "classe": "card-6-espadas"
+    },
+    {
+        "valor": "7",
+        "naipe": 2,
+        "classe": "card-7-espadas"
+    },
+    {
+        "valor": "9",
+        "naipe": 0,
+        "classe": "card-9-ouros"
+    },
+    {
+        "valor": "10",
+        "naipe": 2,
+        "classe": "card-10-espadas"
+    },
+    {
+        "valor": "1",
+        "naipe": 1,
+        "classe": "card-a-copas"
+    },
+    {
+        "valor": "13",
+        "naipe": 3,
+        "classe": "card-k-paus"
+    },
+    {
+        "valor": "8",
+        "naipe": 3,
+        "classe": "card-8-paus"
+    },
+    {
+        "valor": "2",
+        "naipe": 3,
+        "classe": "card-2-paus"
+    },
+    {
+        "valor": "5",
+        "naipe": 2,
+        "classe": "card-5-espadas"
+    },
+    {
+        "valor": "9",
+        "naipe": 3,
+        "classe": "card-9-paus"
+    },
+    {
+        "valor": "1",
+        "naipe": 3,
+        "classe": "card-a-paus"
+    },
+    {
+        "valor": "6",
+        "naipe": 3,
+        "classe": "card-6-paus"
+    },
+    {
+        "valor": "4",
+        "naipe": 0,
+        "classe": "card-4-ouros"
+    },
+    {
+        "valor": "3",
+        "naipe": 2,
+        "classe": "card-3-espadas"
+    },
+    {
+        "valor": "3",
+        "naipe": 0,
+        "classe": "card-3-ouros"
+    },
+    {
+        "valor": "11",
+        "naipe": 3,
+        "classe": "card-j-paus"
+    },
+    {
+        "valor": "12",
+        "naipe": 0,
+        "classe": "card-q-ouros"
+    },
+    {
+        "valor": "13",
+        "naipe": 1,
+        "classe": "card-k-copas"
+    },
+    {
+        "valor": "5",
+        "naipe": 0,
+        "classe": "card-5-ouros"
+    },
+    {
+        "valor": "9",
+        "naipe": 1,
+        "classe": "card-9-copas"
+    },
+    {
+        "valor": "13",
+        "naipe": 2,
+        "classe": "card-k-espadas"
+    },
+    {
+        "valor": "7",
+        "naipe": 0,
+        "classe": "card-7-ouros"
+    },
+    {
+        "valor": "11",
+        "naipe": 1,
+        "classe": "card-j-copas"
+    },
+    {
+        "valor": "10",
+        "naipe": 3,
+        "classe": "card-10-paus"
+    }
+]
